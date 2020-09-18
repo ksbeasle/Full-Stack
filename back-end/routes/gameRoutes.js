6963eea80036fc9f9a5370073c45c02efaa1e9f5
Kahlil Beasley <ksbeasle@asu.edu>
2020-09-17 18:20:27 -0700

    Need to find way NOT to send the test data into the DB

--- back-end/routes/gameRoutes.js
+++ back-end/routes/gameRoutes.js
@@ -6,7 +6,7 @@ let router = Router()
 router.get('/', gameController.all_get)
 router.get('/game/:id',gameController.get_game)
 router.put('/game/add', gameController.add_game)
-router.put('/game/update/:id', gameController.update_game)
+router.patch('/game/update/:id', gameController.update_game)
 router.delete('/game/delete/:id', gameController.delete_game)
 
 
--- back-end/test.js
+++ back-end/test.js
@@ -31,11 +31,29 @@ describe('Test gameRoutes', () => {
 
     it('GET: Should get a specific game based on the ID', async () => {
         var result = await chai.request(app).get('/game/5f5d2d6896fb5d54cfda3138')
+        console.log(result.body.genre)
         expect(result).to.have.status(200)
         expect(result.body._id).to.equal('5f5d2d6896fb5d54cfda3138')
         expect(result.body.platform[0]._id).to.equal('5f5d2d6896fb5d54cfda3139')
+        expect(result.body.platform[0]).to.include.keys('platform')
         expect(result.body.genre[0]._id).to.equal('5f5d2d6896fb5d54cfda313a')
+        expect(result.body.genre[0]).to.include.keys('genre')
         expect(result.body).to.be.a('object')
         expect(result.body).to.include.keys('title', 'platform', 'genre', 'esrb', 'rating')
+    })
+
+    it('PUT: Should add a game', async () => {
+        var result = await chai.request(app).put('/game/add').send({
+            "title": "test999",
+            "platform": [
+              {"platform": "Xbox"}
+            ],
+            "genre": [ 
+               {"genre": "FPS"}
+             ],
+            "esrb": "esrb",
+            "rating": 1
+          })
+        console.log(result)
     })
     })
